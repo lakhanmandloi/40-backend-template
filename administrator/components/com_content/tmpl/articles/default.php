1be6fef9cee28638f331017a4af28b4837147918
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2018-05-22 22:44:14 +0530

    Issue #436 task: Remove Sidebar

--- administrator/components/com_content/tmpl/articles/default.php
+++ administrator/components/com_content/tmpl/articles/default.php
@@ -62,12 +62,7 @@ $featuredButton = (new ActionButton(['tip_title' => 'JGLOBAL_TOGGLE_FEATURED']))
 
 <form action="<?php echo JRoute::_('index.php?option=com_content&view=articles'); ?>" method="post" name="adminForm" id="adminForm">
 	<div class="row">
-		<?php if (!empty($this->sidebar)) : ?>
-		<div id="j-sidebar-container" class="col-md-2">
-			<?php echo $this->sidebar; ?>
-		</div>
-		<?php endif; ?>
-		<div class="<?php if (!empty($this->sidebar)) {echo 'col-md-10'; } else { echo 'col-md-12'; } ?>">
+		<div class="col-md-12">
 			<?php
 			// Search tools bar
 			echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this));
